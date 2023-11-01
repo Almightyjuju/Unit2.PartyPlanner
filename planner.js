28a7b6cd6d634077eae28b39dbb094cba9313f0d
Almightyjuju <alexanderpizzati11@gmail.com>
2023-10-31 22:14:06 -0500

    finished workshop 21

--- planner.js
+++ planner.js
@@ -1,57 +1,72 @@
 const COHORT = "2309-FTB-ET-WEB-FT";
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
-const parties = [];
+const API = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/" + COHORT;
 
-const addEventForm = document.querySelector("#addEvent");
-const eventsList = document.querySelector("#events");
+const state = {
+  events: [],
+};
 
-// Add event listener for form submission
-addEventForm.addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent form submission
+const partyList = document.getElementById("party-list");
+const partyForm = document.getElementById("party-form");
 
-  // Get the form input values
-  const name = addEventForm.querySelector('input[name="name"]').value;
-  const date = addEventForm.querySelector('input[name="date"]').value;
-  const time = addEventForm.querySelector('input[name="time"]').value;
-  const location = addEventForm.querySelector('input[name="location"]').value;
-  const description = addEventForm.querySelector(
-    'input[name="description"]'
-  ).value;
+async function createEvent(event) {
+  event.preventDefault();
+  // 2023-10-31T14:50 + ":00.000Z"
+  // 2021-09-30T00:00:00.000Z, // Date ISO string
 
-  const newEvent = {
-    name,
-    date,
-    time,
-    location,
-    description,
-  };
-  addEvent(event);
-
-  addEventForm.reset();
-});
-
-function addEvent(event) {
-  const listItem = document.createElement("li");
-
-  listItem.innerHTML = `
-<strong>${event.name}</strong><br>
-Date: ${event.date}<br>
-Time: ${event.time}<br>
-Location: ${event.location}<br>
-Description: ${event.description}`;
-
-  eventsList.appendChild(listItem);
-}
-
-//----------------------------------------------
-async function getEvents() {
   try {
-    let response = await fetch(API_URL);
-    let data = await response.json();
-
-    parties = data.data;
-    console.log(parties);
+    const response = await fetch(API + "/events", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        name: document.getElementById("name").value,
+        description: document.getElementById("description").value,
+        date: `${document.getElementById("date").value}:00.000Z`,
+        location: document.getElementById("location").value,
+      }),
+    });
+    getEvents();
   } catch (err) {
     console.error(err);
   }
 }
+
+partyForm.addEventListener("submit", createEvent);
+
+async function getEvents() {
+  try {
+    const response = await fetch(API + "/events");
+    const json = await response.json();
+    state.events = json.data;
+    render();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function render() {
+  const events = state.events.map((event) => {
+    const article = document.createElement("article");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "X";
+    deleteBtn.addEventListener("click", async () => {
+      try {
+        const response = await fetch(API + `/events/${event.id}`, {
+          method: "DELETE",
+        });
+        console.log("deleted!");
+        getEvents();
+      } catch (err) {
+        console.error(err);
+      }
+    });
+    article.innerHTML = `
+         <h3>${event.name}</h3>
+         <address>${event.location}</address>`;
+    article.append(deleteBtn);
+
+    return article;
+  });
+  partyList.replaceChildren(...events);
+}
+
+getEvents();
